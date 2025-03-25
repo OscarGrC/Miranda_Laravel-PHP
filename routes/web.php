9583e30e90ede3a9f858cf58e3d46dfc1cd90bbf
Oscar gracia <113419592+OscarGrC@users.noreply.github.com>
2025-03-25 14:31:26 +0000

    blezee blade

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ActivitiesController;
 use App\Http\Controllers\UsersController;
-Route::resource('activities', ActivitiesController::class);
-Route::resource('users', UsersController::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('activities', ActivitiesController::class);
+    Route::resource('users', UsersController::class);
+});
+
+require __DIR__.'/auth.php';
